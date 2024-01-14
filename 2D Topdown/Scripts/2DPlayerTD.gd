53c6c9febdce132a9324bfdd823b9898a63aaac6
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-01-14 16:26:50 -0300

    Update Animation TopDown

--- 2D Topdown/Scripts/2DPlayerTD.gd
+++ 2D Topdown/Scripts/2DPlayerTD.gd
@@ -5,7 +5,8 @@ extends CharacterBody2D
 @onready var gravityVector = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
 @onready var walk := Vector2.ZERO
 @onready var fallDirection := Vector2.ZERO
-
+@onready var spr := $SprPlayer
+@onready var anim := $SprAnimation
 func _physics_process(delta):
 	GravitySystem(delta)
 	Walk()
